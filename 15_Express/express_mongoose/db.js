ae6c136f8e9186660b10c9fe6fe2dbd3a92f1a22
Michal Broos <2330994b@gmail.com>
2022-06-08 17:55:11 +0100

    Complete db in Express Mongoose exercise

--- 15_Express/express_mongoose/db.js
+++ 15_Express/express_mongoose/db.js
@@ -1,1 +1,33 @@
 "use strict"
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/movies_exercise", {useNewUrlParser: true}, (err) => {
+    return err ? console.log("db connection error") : console.log("db connected");
+});
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true},
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    earnings: {
+        type: Number,
+        min: 0,
+        max: Number.MAX_SAFE_INTEGER,
+        default: 0
+    },
+    actors: {
+        type: [String],
+        default: ["default actor 1", "default actor 2"]
+    }
+});
+
+const Movie = mongoose.model("movie", movieSchema);
+
+module.exports = Movie;
