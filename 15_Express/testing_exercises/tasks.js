cd71e7f822e8698925432f3c46ad9b16865748b2
Michal Broos <2330994b@gmail.com>
2022-06-09 13:04:52 +0100

    Start testing exercise Task 2 Code before lunch

--- 15_Express/testing_exercises/tasks.js
+++ 15_Express/testing_exercises/tasks.js
@@ -19,9 +19,32 @@ let isFactorial = (intToTest) => {
 };
 
 const testInts = [120, 150, 3628800, 479001600, 6, 18];
-for (let testInt of testInts) {
-    console.log(isFactorial(testInt));
+// for (let testInt of testInts) {
+//     console.log(isFactorial(testInt));
+// }
+
+// Task 2
+let doggoCompetition = (place) => {
+    // let places = [...Array(100).keys()]
+    // places.splice(--place, 1);
+    // console.log(places);
+
+    for (let i = 1; i < 101; i++) {
+        switch (i) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 100:
+                break;
+            default:
+                //
+        }
+    }
 }
+doggoCompetition(1);
 
 module.exports = {
     isFactorial
