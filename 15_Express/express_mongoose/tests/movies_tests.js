e9cdd5a1782984726ec7951256bc0fbb5ff1d0fe
Michal Broos <2330994b@gmail.com>
2022-06-10 17:35:22 +0100

    Add getById test for movies tests Also includes a done() fix and a typo fix

--- 15_Express/express_mongoose/tests/movies_tests.js
+++ 15_Express/express_mongoose/tests/movies_tests.js
@@ -22,19 +22,31 @@ mocha.describe("Movies CRUD testing", () => {
                 }).then((createdMovie) => {
                     if (i === 0) {
                         id = createdMovie._id;
+                        done();
                     }
                     // done() here would run it 5 times = wrong!
                 }).catch((err) => console.error(err));
             }
-            done();
         }).catch((err) => console.error(err));
     });
 
-    mocha.it("should get all 5 trainers", (done) => {
+    mocha.it("should get all 5 movies", (done) => {
         chai.request(server).get("/movies/getAll").end((err, res) => {
             chai.expect(err).to.be.null;
             chai.expect(res.status).to.equal(200);
             chai.expect(res.body.length).to.equal(5);
+            done();
+        });
+    });
+
+    mocha.it(`should get movie "title0" (first movie)`, (done) => {
+        chai.request(server).get(`/movies/getById/${id}`).end((err, res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res.status).to.equal(200);
+            // console.log(id);
+            // console.log(res.req._header);
+            // console.log(res.req.path);
+            chai.expect(res.body.title).to.equal("title0");
             done();
         });
     });
