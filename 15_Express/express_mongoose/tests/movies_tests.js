4e92ad370b7a45122a5033d3ea458d9fe6fe634d
Michal Broos <2330994b@gmail.com>
2022-06-10 16:30:24 +0100

    Add beforeEach and getAll test for testing movies

--- 15_Express/express_mongoose/tests/movies_tests.js
+++ 15_Express/express_mongoose/tests/movies_tests.js
@@ -8,10 +8,34 @@ chai.use(chaiHttp);
 const server = require("../index.js");
 const Movie = require("../db.js");
 
-Mocha.describe("Movies CRUD testing", () => {
-    mocha.beforeEach(() => {
+mocha.describe("Movies CRUD testing", () => {
+    let id;
+
+    mocha.beforeEach((done) => {
+        Movie.deleteMany({}).then(() => {
+            for (let i = 0; i < 5; i++) {
+                Movie.create({
+                    title: `title${i}`,
+                    releaseDate: `${1995+i}, 11, 17`,
+                    earnings: `99999${i}`,
+                    actors: [`a${i}`, `b${i}`]
+                }).then((createdMovie) => {
+                    if (i === 0) {
+                        id = createdMovie._id;
+                    }
+                    // done() here would run it 5 times = wrong!
+                }).catch((err) => console.error(err));
+            }
+            done();
+        }).catch((err) => console.error(err));
     });
 
-    mocha.it("should ", () => {
+    mocha.it("should get all 5 trainers", (done) => {
+        chai.request(server).get("/movies/getAll").end((err, res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res.status).to.equal(200);
+            chai.expect(res.body.length).to.equal(5);
+            done();
+        });
     });
 });
