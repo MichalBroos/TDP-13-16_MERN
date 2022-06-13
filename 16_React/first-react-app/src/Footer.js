2e13976727e03377d4b9ff4a8a67b826ff37874d
MichalBroos <46611468+MichalBroos@users.noreply.github.com>
2022-06-13 16:27:13 +0100

    Add a note regarding fragments

--- 16_React/first-react-app/src/Footer.js
+++ 16_React/first-react-app/src/Footer.js
@@ -5,6 +5,9 @@ import React from 'react';
 // it's just 2 types of => function (single vs multi line)
 const Footer = () => (
     // React.Fragment required because o/w cannot return multiple elements
+    // alternatively, use the short syntax of <> and </>
+    // see https://reactjs.org/docs/fragments.html +
+    // https://www.jackfranklin.co.uk/blog/react-fragments/
     <React.Fragment>
         <p>p1</p>
         <p>p2</p>
