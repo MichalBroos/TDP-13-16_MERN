e7dee4530bc51bc031fcbf92b8bbd89517d00fea
Michal Broos <2330994b@gmail.com>
2022-06-16 12:54:38 +0100

    Solve React Data Requests exercise Search based on user input works Includes last second fix of resetting loaded between searches TODO: Code optimisation/tweaking + potential message formatting

--- 16_React/first-react-app/src/Film.js
+++ 16_React/first-react-app/src/Film.js
@@ -1,11 +1,15 @@
 const Film = ({name, actors, release}) => {
-    return (
-        <>
-            <h3>{name}</h3>
-            <p>Actors: {actors}</p>
-            <p>Release: {release}</p>
-        </>
-    )
+    if (name) {
+        return (
+            <>
+                <h3>{name}</h3>
+                <p>Actors: {actors}</p>
+                <p>Release: {release}</p>
+            </>
+        )
+    } else {
+        return <p>No such film found</p>;
+    }
 }
 
 export default Film;
--- 16_React/first-react-app/src/FilmRequest.js
+++ 16_React/first-react-app/src/FilmRequest.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import Film from './Film.js';
 
@@ -6,9 +6,13 @@ const FilmRequest = () => {
     const [error, setError] = useState(null);
     const [loaded, setLoaded] = useState(false);
     const [film, setFilm] = useState();
+    const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        axios.get("http://www.omdbapi.com/?apikey=[secret]&t=harry")
+    const searchFilm = () => {
+        // reset required to show Loading... before result is shown
+        setLoaded(false);
+
+        axios.get(`http://www.omdbapi.com/?apikey=[secret]&t=${search}`)
             .then(res => {
                 setLoaded(true);
                 setFilm(res.data);
@@ -19,15 +23,39 @@ const FilmRequest = () => {
                     setError(err);
                 }
             );
-    }, []);
+    }
+
+    const topElements = (
+        <>
+            <h1>Film search</h1>
+            <input
+                type="text" name="search"
+                value={search} onChange={e => setSearch(e.target.value)}
+            />
+            <button onClick={searchFilm}>Search</button>
+        </>
+    );
 
     if (error) {
-        return <div>Error: {error.message}</div>
+        return (
+            <>
+                {topElements}
+                <div>Error: {error.message}</div>
+            </>
+        );
     } else if (!loaded) {
-        return <div>Loading...</div>
+        return (
+            <>
+                {topElements}
+                {film ? <p>Loading...</p> : <p>No results yet...</p>}
+            </>
+        );
     } else {
         return (
-            <Film key={film.imdbID} name={film.Title} actors={film.Actors} release={film.Released}/>
+            <>
+                {topElements}
+                <Film key={film.imdbID} name={film.Title} actors={film.Actors} release={film.Released}/>
+            </>
         );
     }
 }
